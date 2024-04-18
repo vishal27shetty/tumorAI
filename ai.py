2c0154a48b1297647d02d6eca7325e9ffc43e485
vishal27shetty <vishal27shetty@gmail.com>
2024-04-19 00:08:30 +0530

    AI

---
+++ ai.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import tensorflow as tf
+dataset = pd.read_csv('cancer.csv')
+x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])
+y = dataset["diagnosis(1=m, 0=b)"]
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=100)
